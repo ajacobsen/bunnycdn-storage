907ba998299d924ec600d2ae7f87b17da7acb06a
Danjo <atze.danjo@gmail.com>
2020-07-23 21:34:22 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bunnycdn-storage',
+    version='0.1',
+    description='A python module for the BunnyCDN Storage API',
+    url='',
+    author='Andy Jacobsen',
+    author_email='atze.danjo@gmail.com',
+    license='GPLv3+',
+    packages=['bunnycdn_storage'],
+    zip_safe=False)
