f0db19194f3c8ae4af1150b9befa848d7951bd6e
Danjo <atze.danjo@gmail.com>
2020-07-28 03:02:21 +0200

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bunnycdn-storage',
-    version='0.2',
+    version='0.3',
     description='A python module for the BunnyCDN Storage API',
     url='https://github.com/ajacobsen/bunnycdn-storage',
     author='Andy Jacobsen',
